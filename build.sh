b894ec7d4b1fb555f5fac710364130bc7e3c8358
arasakumar786 <arasakumararasan@gmail.com>
2026-06-08 21:01:56 +0530

    comiting the changes for modified fiels

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 IMAGE_NAME="nginx-app"
 IMAGE_TAG="latest"
@@ -6,7 +7,5 @@ IMAGE_TAG="latest"
 echo "Building image with tag: $IMAGE_TAG"
 
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
-
-docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
 
 echo "Build completed: ${IMAGE_NAME}:${IMAGE_TAG}"
